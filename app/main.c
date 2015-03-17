7315fe0f5a9b133d0965debc67b79f79c97e0ab6
miton <rem.coll18@gmail.com>
2015-03-17 11:08:35 +0100

    add .dll

--- app/main.c
+++ app/main.c
@@ -4,15 +4,7 @@ int main(int argc, char* argv[])
 {
 	int e1, e2;
 
-	if( argc == 3)
-	{
-		printf("lancement mode console:");
-		//e1 = argv[1];
-		//e2 = argv[2];
+	ihm( &e1, &e2);
 
-	}
-	else {
-		ihm( &e1, &e2);
-	}
 	return 0;
 }
