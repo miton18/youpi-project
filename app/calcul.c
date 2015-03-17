b81a61d7e6873963d3e4ec9db54d2ec2103aa1fe
guy-yann <refranguy@hotmail.fr>
2015-03-17 10:47:33 +0100

    Modif calcul.c

---
+++ app/calcul.c
@@ -0,0 +1,8 @@
+#include "calcul.h"
+
+int calcul(int*a, int*b)
+{
+	int resultat = 0;
+	resultat = a+b;
+	return resultat;
+}
--- app/calcul.h
+++ app/calcul.h
@@ -1,1 +1,7 @@
-#include "declaration.h"
+#ifndef CALCUL_H
+#define CALCUL_H
+	
+#endif
+
+
+int calcul(int *a, int*b);
