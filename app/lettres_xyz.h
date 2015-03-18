c932f7d5a0b52e3d85816ac7261e9cd19a1b5dec
guy-yann <refranguy@hotmail.fr>
2015-03-18 17:55:23 +0100

    fonction lettre x 'fini'

--- app/lettres_xyz.h
+++ app/lettres_xyz.h
@@ -6,16 +6,16 @@
 #ifndef LETTRESXYZ_H
 #define LETTRESXYZ_H
 
-
+   	#include <math.h>
 	void lettresXYZ(char* str, float* tx, float* ty,float* tz,int* ttr,int* np);
 	
 	//Génération des points pour les differents chiffres romains
 	//return le nombre de point
-	int lettreX(float* x, float* y,float* z,int* tr);
-	int lettreI(float* x, float* y,float* z,int* tr);
-	int lettreM(float* x, float* y,float* z,int* tr);
-	int lettreV(float* x, float* y,float* z,int* tr);
-	int lettreC(float* x, float* y,float* z,int* tr);
-	int lettreD(float* x, float* y,float* z,int* tr);
-	int lettreL(float* x, float* y,float* z,int* tr);
+	int lettreX(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreI(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreM(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreV(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreC(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreD(float* x, float* y,float* z,int* tr,int i,int *indice);
+	int lettreL(float* x, float* y,float* z,int* tr,int i,int *indice);
 #endif
