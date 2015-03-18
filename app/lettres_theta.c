d919c1d46b42f02564f11fba16d8e37f757ea963
miton <rem.coll18@gmail.com>
2015-03-18 15:02:53 +0100

    debut tt

--- app/lettres_theta.c
+++ app/lettres_theta.c
@@ -4,3 +4,19 @@
    	Header Fonction lettresTheta
  -----------------------------------------------------*/
 #include "lettres_theta.h"
+
+void lettresTheta(float* tx, float* ty, float* tz, int np, float* tt1, float* tt2, float* tt3)
+{
+	//ALLOCATION MEMOIRE DES TABLEAUX -- MEMOIRE MAX
+	tt1 = malloc( sizeof(float) * 400 * np ); //nombre de point Max x taille float x nombre d'indices
+	tt2 = malloc( sizeof(float) * 400 * np );
+	tt3 = malloc( sizeof(float) * 400 * np );
+
+
+	for (int i=0; i< np; i++) // tout les indices du/des tableaux
+	{
+		tt1[i]= ;
+		tt2[i]= ;
+		tt3[i]= ;
+	}
+}
--- app/lettres_theta.h
+++ app/lettres_theta.h
@@ -5,5 +5,8 @@
 -----------------------------------------------------*/
 #ifndef LETTRESTHETA
 #define LETTRESTHETA
+
+	#include <stdlib.h>
+
    	void lettresTheta(float* tx, float* ty, float* tz, int np, float* tt1, float* tt2, float* tt3);
 #endif
