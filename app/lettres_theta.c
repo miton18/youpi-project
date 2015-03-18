cedbcf43e32488fd06e71e6b23a03fbd5fcb6ad5
miton <rem.coll18@gmail.com>
2015-03-18 17:56:34 +0100

    lettre theta en cours de correction

--- app/lettres_theta.c
+++ app/lettres_theta.c
@@ -5,6 +5,7 @@
  -----------------------------------------------------*/
 
 #include "lettres_theta.h"
+#include <stdio.h>
 
 
 void lettresTheta(float* tx, float* ty, float* tz, int np, float* tt1, float* tt2, float* tt3)
@@ -12,8 +13,20 @@ void lettresTheta(float* tx, float* ty, float* tz, int np, float* tt1, float* tt
 
 	for (int i=0; i< np; i++) // tout les indices du/des tableaux
 	{
-		tt1[i]= (float) atan( - tz[i] / tx[i]);
-		tt2[i]= (float) -acos(1);
-		tt3[i]= (float) 2.0;
+		//printf("x: %f y: %f z: %f\n", tx[i], ty[i], tz[i]); //OK
+
+		tt1[i]= atan( - tz[i] / tx[i]) * 57.295779513082;
+		tt3[i]= acos(
+							(  pow(tx[i],2)
+							 + pow(tz[i],2)
+							 + pow((ty[i] - L1),2)
+							 - pow(L2,2)
+							 - pow(L3,2) )/ ( 2 * L2 * L3 )) * -57.295779513082;
+		tt2[i]= -0.5 * (acos(
+							( pow(tx[i],2) + pow(tz[i],2) - pow((ty[i]-L1),2) )
+							/
+							( pow(tx[i],2) + pow(tz[i],2) + pow((ty[i]-L1),2) )
+						)* 57.295779513082 + tt3[i]);
 	}
+	printf("#######TEST: 1: %f, 2: %f, 3: %f", tt1[0], tt2[0], tt3[0]);
 }
---
+++ app/test-lettres_theta.c
@@ -0,0 +1,34 @@
+#include "lettres_theta.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+	int x = atoi( argv[1]);
+	int y = atoi( argv[2]);
+	int z = atoi( argv[3]);
+
+	float * tx = malloc( sizeof(float) * 400 * 16 );
+	float * ty = malloc( sizeof(float) * 400 * 16 );
+	float * tz = malloc( sizeof(float) * 400 * 16 );
+	tx[0] = x;
+	ty[0] = y;
+	tz[0] = z;
+
+	//printf("tx0 %f\n", tx[0]);
+
+
+	float * tt1 = malloc( sizeof(float) * 400 * 16 ); //nombre de point Max x taille float x nombre de caractere max
+	float * tt2 = malloc( sizeof(float) * 400 * 16 );
+	float * tt3 = malloc( sizeof(float) * 400 * 16 );
+
+
+
+
+
+
+
+	lettresTheta(tx, ty, tz, 1, tt1, tt2, tt3);
+
+	printf("\nVALEUR DE RETOUR: T1: %f T2: %f T3: %f\n", tt1[0], tt2[0], tt3[0]);
+}
