ecc020809b76a38fbdad7863137307a8f5c5161d
miton <rem.coll18@gmail.com>
2015-03-18 15:22:58 +0100

    theta
    3

--- app/lettres_theta.h
+++ app/lettres_theta.h
@@ -9,5 +9,11 @@
 	#include <stdlib.h>
 	#include <math.h>
 
+	#define L1 175
+	#define L2 160
+	#define L3 160
+
    	void lettresTheta(float* tx, float* ty, float* tz, int np, float* tt1, float* tt2, float* tt3);
+
+
 #endif
