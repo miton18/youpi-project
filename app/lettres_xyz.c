9e87d6ddfbf4ca2470dacf59529e04a42ef4ed6e
guy-yann <refranguy@hotmail.fr>
2015-03-18 11:38:41 +0100

    rename

--- app/lettres_xyz.c
+++ app/lettres_xyz.c
@@ -5,3 +5,11 @@
    	Création des trajectoires en x,y,z 
    	-----------------------------------------------------*/
 #include "lettresXYZ.h"
+
+   	void lettresXYZ(char* str, float* tx, float* ty,float* tz,int* ttr,int* np){
+   		return 0;
+   	}
+
+   	int lettreX(float* x, float* y,float* z,int* tr){
+   		
+   	}
