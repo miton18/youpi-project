b9cf615eb8cf79883325dd764f2eb44525b50d68
guy-yann <refranguy@hotmail.fr>
2015-03-18 11:03:39 +0100

    ajout commentaire et test si =0 dans calcul

--- app/lettresXYZ.c
+++ app/lettresXYZ.c
@@ -2,5 +2,6 @@
    Auteur : Collignon Rémi
    			Morvan Guy-Yann
    	Fonction lettresXYZ
+   	Création des trajectoires en x,y,z 
    	-----------------------------------------------------*/
 #include "lettresXYZ.h"
