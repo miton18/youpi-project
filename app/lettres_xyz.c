cac90e5b8c39b63d422a6b65992ebc4ce7cdcb41
guy-yann <refranguy@hotmail.fr>
2015-03-31 18:00:38 +0200

    yolo sat swag

--- app/lettres_xyz.c
+++ app/lettres_xyz.c
@@ -276,29 +276,34 @@
       int lettreD(float* x, float* y,float* z,int* tr,int i,int* indice){
          int nbp=200;
          float j;
-         float pas1 = 11/(0.35*(float)nbp);
-         float pas2 = M_PI*180/(0.75*(float)nbp*180);
-
+         float pas1 = 11/(0.25*(float)nbp);
+         float pas2 = 180/(0.75*(float)nbp);
+         int trace =1;
+         // tracé points de l'ellipse
+         for ( j = 0; j < 180; j=j+pas2)
+         {
+            x[*indice]=5.5*cos(M_PI*j/180)+150.5;
+            y[*indice]=50;
+            z[*indice]=9*sin(M_PI*j/180)+(i*18)-139.5;
+            if (trace == 1){
+               tr[*indice]=0; 
+               trace = 0;
+            }
+            else{
+               tr[*indice]=1;
+            }
+            *indice=*indice+1;
+         }
          //tracé points du trait vertical
-         for (j = 0; j < 11; j=j+pas1)
+         for (j = 0; j < 11+pas1; j=j+pas1)
          {
             x[*indice]=j+145;
             y[*indice]=50;
             z[*indice]=0+(i*18)-139.5;
             tr[*indice]=1;
-            *indice=*indice+1;      
-         }
+            *indice=*indice+1;    
+         }    
 
-         // tracé points de l'ellipse
-         for ( j = 0; j < 3.14159265; j=j+pas2)
-         {
-            x[*indice]=5.5*cos(j)+150.5;
-            y[*indice]=50;
-            z[*indice]=9*sin(j)+(i*18)-139.5;
-            tr[*indice]=1;
-            *indice=*indice+1;
-         }
-         tr[*indice-1]=0;
          return nbp;
       }
 
@@ -316,8 +321,9 @@
             if(trace == 1){
                tr[*indice]=0;
                trace = 0;
+            }else{
+               tr[*indice]=1;
             }
-            tr[*indice]=1;
             *indice=*indice+1;
          }
          //tracé points du trait orizontal
@@ -329,6 +335,5 @@
             tr[*indice]=1;
             *indice=*indice+1;
          }
-         tr[*indice-1]=0;
          return nbp;
       }
