4085a2a3579a536fbf06d3af89f00b3221db6835
guy-yann <refranguy@hotmail.fr>
2015-03-31 16:29:08 +0200

    C'est bon za

--- app/lettres_xyz.c
+++ app/lettres_xyz.c
@@ -4,6 +4,7 @@
    	Fonction lettresXYZ
    	Création des trajectoires en x,y,z 
    	-----------------------------------------------------*/
+
 #include "lettres_xyz.h"
 
    	void lettresXYZ(char* str, float* tx, float* ty,float* tz,int* ttr,int* np){
@@ -51,15 +52,15 @@
 
    	int lettreX(float* x, float* y,float* z,int* tr,int i,int* indice){
          int nbp=202;
-         float pas = 9*2/((float) nbp);
+         float pas = 9*2/((float) nbp-2);
 /*         float hypo=sqrt(pow(9,2)+pow(11,2));
          float pas=hypo*2/((float)nbp);*/
          float j;
          int trace = 1;
          //tracé points deuxieme diagonale
-         for (j = 0; j < 9; j=j+pas)
+         for (j = 0; j < 9+pas; j=j+pas)
          {
-            x[*indice]=(11/9)*j +145;
+            x[*indice]=(11*j/9) +145;
             y[*indice]=50;
             z[*indice]=j+(i*18)-139.5;
             if (trace==1){
@@ -71,11 +72,16 @@
             }
             *indice=*indice+1;
          }
+         x[*indice]=(11*9/9)+145;
+         y[*indice]=50;
+         z[*indice]=9+(i*18)-139.5;
+         tr[*indice]=1;
+         *indice=*indice+1;
          trace =1;
          //tracé points première diagonale
-         for (j = 0; j < 9;j=j+pas )
+         for (j = 0; j < 9+pas;j=j+pas )
          {
-            x[*indice]=((-11/9)*j+11)+145;
+            x[*indice]=((-11*(j-9))/9)+145;
             y[*indice]=50;
             z[*indice]=j+(i*18)-139.5;
             if (trace==1){
@@ -87,6 +93,11 @@
             }
             *indice=*indice+1;
          }
+         x[*indice]=((-11*(9-9))/9)+145;
+         y[*indice]=50;
+         z[*indice]=9+(i*18)-139.5;
+         tr[*indice]=1;
+         *indice=*indice+1;
          return nbp;
 
    	}
@@ -239,18 +250,23 @@
       int lettreC(float* x, float* y,float* z,int* tr,int i,int* indice){
          int nbp=200;
          float j;
-         float pas = 180/((float)nbp);
-
+         float pas = M_PI*220/((float)nbp*180);
+         int trace =1;
          //tracé point de l'ellipse
-         for ( j = 0; j < 181; j=j+pas)
+         for ( j = 2.7925268; j < 6.63225116 ; j=j+pas)
          {
-            x[*indice]=5.5*cos(j)+145;
+            x[*indice]=5.5*cos(j)+150.5;
             y[*indice]=50;
-            z[*indice]=4.5*sin(j)+(i*18)-139.5;
-            tr[*indice]=1;
+            z[*indice]=4.5*sin(j)+(i*18)-135;
+            if (trace == 1){
+               tr[*indice]=0; 
+               trace = 0;
+            }
+            else{
+               tr[*indice]=1;
+            }
             *indice=*indice+1;
          }
-         tr[*indice-1]=0;
          return nbp;
       }
 
@@ -258,7 +274,7 @@
          int nbp=200;
          float j;
          float pas1 = 11/(0.35*(float)nbp);
-         float pas2 = 180/(0.75*(float)nbp);
+         float pas2 = M_PI*180/(0.75*(float)nbp*180);
 
          //tracé points du trait vertical
          for (j = 0; j < 11; j=j+pas1)
@@ -271,9 +287,9 @@
          }
 
          // tracé points de l'ellipse
-         for ( j = 0; j > -180; j=j-pas2)
+         for ( j = 0; j < 3.14159265; j=j+pas2)
          {
-            x[*indice]=5.5*cos(j)+145;
+            x[*indice]=5.5*cos(j)+150.5;
             y[*indice]=50;
             z[*indice]=9*sin(j)+(i*18)-139.5;
             tr[*indice]=1;
